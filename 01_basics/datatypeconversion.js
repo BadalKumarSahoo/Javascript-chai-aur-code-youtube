01146d76f957430b1452460dfba5f198525bf0d8
Badal Kumar Sahoo <sahoobadalkumar94@gmail.com>
2024-07-24 04:57:03 +0000

    Conversion of datatypes

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,16 @@
+// let n="66abc"
+// console.log(typeof n);
+// let nu=Number(n);//Number is used for conversion of any data type to Number datatype
+// console.log(typeof nu);
+// console.log(nu);
+// let nt=NaN(n);
+// console.log(typeof nt);
+// console.log(nt);
+//Change number data type
+//true=>1,false=>0,undefined=>NaN,empty string=>0
+//   Boolean
+//1=>True,0=>False,empty string=>false, any thing=>True 
+let IsString= true;
+let Changestring= String(IsString);
+console.log(Changestring);
+console.log(typeof Changestring);
